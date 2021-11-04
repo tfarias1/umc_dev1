ec652e23947492ec95955b6205d02b96858eddd1
tfarias1 <thiagofarias1@gmail.com>
2021-11-03 21:09:36 -0300

    Acrescentado nova saída de dados via console

--- Umc_Dev1/src/umc_dev1/Umc_Dev1.java
+++ Umc_Dev1/src/umc_dev1/Umc_Dev1.java
@@ -4,6 +4,7 @@ public class Umc_Dev1 {
 
     public static void main(String[] args) {
         System.out.println("Aula3");
+        System.out.println("Hoje fizemos o teste de GIT");
     }
     
 }
