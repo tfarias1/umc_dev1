779fd4f295cc32cb8671816264f9bafbb5625113
Kevin <Kevin@192.168.0.122>
2021-11-04 12:16:13 -0300

    Criei exemplo de Abstracao

---
+++ Umc_Dev1/src/umc_dev1/abstracao/Animal.java
@@ -0,0 +1,30 @@
+
+package umc_dev1.abstracao;
+
+abstract class Animal {
+ 
+  public abstract void animalSound();
+  
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass 
+class Pig extends Animal {
+  public void animalSound() {
+    
+    System.out.println("O Porco Faz: wee wee");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
+
+
+
